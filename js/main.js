ad898af75f0e62153ba39089318cf1a5e9103bfe
SherryW <sherry94063023@gmail.com>
2026-01-28 00:08:36 +0800

    頁面導向 & bell & home

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 /* main.js */
 
+const leftPage = document.getElementById("leftPage");
+const rightPage = document.getElementById("rightPage");
+
 document.addEventListener("DOMContentLoaded", () => {
     const mainTabs = document.querySelectorAll(".main-tab");
 
@@ -18,4 +21,37 @@ document.addEventListener("DOMContentLoaded", () => {
             group.classList.toggle("open");
         });
     });
+    loadHomePage(); //一打開index.html就載入homepage
 });
+
+function loadPage(path, leftId, rightId) {
+    fetch(path)
+        .then(res => res.text())
+        .then(html => {
+            const temp = document.createElement("div");
+            temp.innerHTML = html;
+
+            leftPage.innerHTML =
+                temp.querySelector(leftId).innerHTML;
+
+            rightPage.innerHTML =
+                temp.querySelector(rightId).innerHTML;
+        });
+}
+
+function loadHomePage() {
+    fetch("BookmarkPages/Home.html")
+        .then(res => res.text())
+        .then(html => {
+            const temp = document.createElement("div");
+            temp.innerHTML = html;
+
+            leftPage.innerHTML =
+                temp.querySelector("#home-left").innerHTML;
+
+            rightPage.innerHTML =
+                temp.querySelector("#home-right").innerHTML;
+        });
+}
+
+
